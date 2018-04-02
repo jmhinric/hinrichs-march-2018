36155fa0f266a1e889d336e9e5148d41ee987b5a
John Hinrichs <jmhinric@gmail.com>
2018-04-01 23:21:41 -0500

    Add nested routes with events

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,16 +1,21 @@
 class PeopleController < ApplicationController
+  before_action :load_event, only: [:create, :new]
+
+  def new
+  end
+
   def create
     ActiveRecord::Base.transaction do
       @person = Person.create(person_params)
       @league = League.where(name: league_name_params).first_or_create!
       @person.update!(league: @league)
-      CreateCheckin.call(@person, Event.last, checkin_params[:weight].to_f, current_user)
+      CreateCheckin.call(@person, @event, checkin_params[:weight].to_f, current_user)
     end
-    redirect_to people_path
+    redirect_to event_people_path
   end
 
   def index
-    @event = Event.includes(:people).last
+    @event = Event.includes(people: :league).find(event_id)
     unless @event
       redirect_to new_event_path
     end
@@ -23,10 +28,20 @@ class PeopleController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:event_id])
     @person = Person.find(params[:id])
   end
 
   private
+
+  def event_id
+    params.permit(:event_id)[:event_id]
+  end
+
+  def load_event
+    @event = Event.find(event_id)
+  end
+
   def person_params
     params.required(:person).permit([:name])
   end
