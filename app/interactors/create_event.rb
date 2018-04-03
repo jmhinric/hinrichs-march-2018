ab327e2ee9f94b7f2a47d68887ab05bd52f69208
John Hinrichs <jmhinric@gmail.com>
2018-04-02 20:27:33 -0500

    Fix spec

--- app/interactors/create_event.rb
+++ app/interactors/create_event.rb
@@ -1,6 +1,14 @@
 class CreateEvent
 
   def self.call(name, tagline)
-    Event.create(name: name, tagline: tagline)
+    event = Event.create(name: name, tagline: tagline)
+    # This code is destroying history, and I would eliminate it.
+    # Person.find_each do |p|
+    #   p.starting_weight = nil
+    #   p.up_by = nil
+
+    #   p.save
+    # end
+    event
   end
 end
